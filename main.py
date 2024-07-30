75ec440329f33a1a88fd74e46e11f9ae58885b3a
yadavau <akash.yadav@urbs.systems>
2024-07-30 17:16:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,49 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+@st.cache_data  # Updated from st.experimental_memo
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+img = get_img_as_base64("image.jpg")
+img2 = get_img_as_base64("photo-1501426026826-31c667bdf23d.jpg")
+
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("data:image/png;base64,{img2}");
+background-size: 300%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 
 def main():
     # Initialize MongoDB client
